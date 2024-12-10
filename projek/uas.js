d6e85b6386db3a3e169e0cf03dff2d0163b0c68f
rangga-create <ranggasaputraaaa453@gmail.com>
2024-12-10 08:41:44 +0700

    first commit

---
+++ projek/uas.js
@@ -0,0 +1,9 @@
+// let nilai=prompt(`masukan nilai ujian kamu`)
+// let hasil=(nilai >=70) ? "nilai sudah baik":"nilai kurang";
+// console.log(`hasil ${hasil}`)
+// let demo = document.getElementById("demo");
+// demo.onclick = showMessage;
+
+// function showMessage() {
+//    alert("Hello, World!");
+// }
